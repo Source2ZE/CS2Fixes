b4c6e2635d1bdce7777fa1a76083c6dcc557ac95
xen <xen@momentum-mod.org>
2023-09-30 13:51:34 +0300

    Fix schema maps

--- cs2_sdk/schema.cpp
+++ cs2_sdk/schema.cpp
@@ -10,7 +10,7 @@
 using SchemaKeyValueMap_t = CUtlMap<uint32_t, int16_t>;
 using SchemaTableMap_t = CUtlMap<uint32_t, SchemaKeyValueMap_t*>;
 
-static bool InitSchemaFieldsForClass(SchemaTableMap_t& tableMap, const char* className, uint32_t classKey)
+static bool InitSchemaFieldsForClass(SchemaTableMap_t *tableMap, const char* className, uint32_t classKey)
 {
     CSchemaSystemTypeScope* pType = interfaces::pSchemaSystem->FindTypeScopeForModule("server.dll");
 
@@ -21,8 +21,8 @@ static bool InitSchemaFieldsForClass(SchemaTableMap_t& tableMap, const char* cla
     if (!pClassInfo)
     {
         //tableMap.emplace(classKey, SchemaKeyValueMap_t{});
-        SchemaKeyValueMap_t *map = new SchemaKeyValueMap_t;
-        tableMap.Insert(classKey, map);
+		SchemaKeyValueMap_t *map = new SchemaKeyValueMap_t(0, 0, DefLessFunc(uint32_t));
+        tableMap->Insert(classKey, map);
 
         Warning("InitSchemaFieldsForClass(): '%s' was not found!\n", className);
         return false;
@@ -31,9 +31,9 @@ static bool InitSchemaFieldsForClass(SchemaTableMap_t& tableMap, const char* cla
     short fieldsSize = pClassInfo->GetFieldsSize();
     SchemaClassFieldData_t* pFields = pClassInfo->GetFields();
 
-    auto& keyValueMap = tableMap[classKey];
-    //keyValueMap.reserve(fieldsSize);
-    
+    SchemaKeyValueMap_t *keyValueMap = new SchemaKeyValueMap_t(0, 0, DefLessFunc(uint32_t));
+	keyValueMap->EnsureCapacity(fieldsSize);
+	tableMap->Insert(classKey, keyValueMap);
 
     for (int i = 0; i < fieldsSize; ++i)
     {
@@ -78,23 +78,23 @@ int16_t schema::FindChainOffset(const char* className)
 
 int16_t schema::GetOffset(const char* className, uint32_t classKey, const char* memberName, uint32_t memberKey)
 {
-    static SchemaTableMap_t schemaTableMap;
-    const auto& tableMapIndex = schemaTableMap.Find(classKey);
-    if (!schemaTableMap.IsValidIndex(tableMapIndex)) // is this correct lol? It's checking if the key is not there and if not it calls below
+	static SchemaTableMap_t schemaTableMap(0, 0, DefLessFunc(uint32_t));
+	int16_t tableMapIndex = schemaTableMap.Find(classKey);
+    if (!schemaTableMap.IsValidIndex(tableMapIndex))
     {
-        if (InitSchemaFieldsForClass(schemaTableMap, className, classKey))
+        if (InitSchemaFieldsForClass(&schemaTableMap, className, classKey))
             return GetOffset(className, classKey, memberName, memberKey);
 
         return 0;
     }
 
-    const SchemaKeyValueMap_t* tableMap = schemaTableMap[tableMapIndex];
-    const auto& memberIndex = tableMap->Find(memberKey);
+    SchemaKeyValueMap_t *tableMap = schemaTableMap[tableMapIndex];
+	int16_t memberIndex = tableMap->Find(memberKey);
     if (!tableMap->IsValidIndex(memberIndex))
     {
         Warning("schema::GetOffset(): '%s' was not found in '%s'!\n", memberName, className);
         return 0;
     }
 
-    return (*tableMap)[memberIndex];
+    return tableMap->Element(memberIndex);
 }
