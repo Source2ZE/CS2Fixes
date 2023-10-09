419086e4bf80f96b4b83a70796950206eeaac384
hardixnph <hardix233@yahoo.com>
2023-10-09 22:19:45 +0200

    feat: add !rs command

--- src/commands.cpp
+++ src/commands.cpp
@@ -403,6 +403,21 @@ CON_COMMAND_CHAT(getstats, "get your stats")
 	ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIX"Damage: %d", stats->m_iDamage.Get());
 }
 
+CON_COMMAND_CHAT(rs, "reset your score")
+{
+	if (!player)
+		return;
+
+	player->m_pActionTrackingServices->m_matchStats().m_iKills = 0;
+	player->m_pActionTrackingServices->m_matchStats().m_iDeaths = 0;
+	player->m_pActionTrackingServices->m_matchStats().m_iAssists = 0;
+	player->m_pActionTrackingServices->m_matchStats().m_iDamage = 0;
+	player->m_iScore = 0;
+	player->m_iMVPs = 0;
+
+	ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIX"You successfully reset your score.");
+}
+
 CON_COMMAND_CHAT(setkills, "set your kills")
 {
 	if (!player)
