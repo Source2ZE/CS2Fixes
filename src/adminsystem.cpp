67e851d9e7361c491c4f12ebf739168f1a6dd7db
mihaigsm2003 <51678688+mihaigsm2003@users.noreply.github.com>
2023-10-19 12:35:17 +0300

    move

--- src/adminsystem.cpp
+++ src/adminsystem.cpp
@@ -747,6 +747,13 @@ CON_COMMAND_CHAT(slay, "slay a player")
 	}
 }
 
+bool caseInsensitiveStringCompare( const std::string& str1, const std::string& str2 ) {
+    std::string str1Cpy( str1 );
+    std::string str2Cpy( str2 );
+    std::transform( str1Cpy.begin(), str1Cpy.end(), str1Cpy.begin(), ::tolower );
+    std::transform( str2Cpy.begin(), str2Cpy.end(), str2Cpy.begin(), ::tolower );
+    return ( str1Cpy == str2Cpy );
+}
 
 CON_COMMAND_CHAT(move, "set a player's team")
 {
