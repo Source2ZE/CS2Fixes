999af2638ca02d96fdf43672eb1ca7ccfed70598
mihaigsm2003 <51678688+mihaigsm2003@users.noreply.github.com>
2023-10-20 22:40:10 +0300

    Update commands.cpp

--- src/commands.cpp
+++ src/commands.cpp
@@ -270,35 +270,7 @@ CON_COMMAND_CHAT(RS, "reset your score")
 }
 //************************************end reset**************************************************************
 //************************************Admins chat**************************************************************
-/*
-CON_COMMAND_CHAT(u, "admins chat")
-{
-	if (!player)
-		return;
 
-	int iCommandPlayer = player->GetPlayerSlot();
-
-	ZEPlayer *pPlayer = g_playerManager->GetPlayer(iCommandPlayer);
-	if (args.ArgC() < 2)
-	{
-		
-		ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIX "Usage: /u <message> to admins");
-		return;
-	}
-
-for (int i = 0; i < MAXPLAYERS; i++)
-{
-    ZEPlayer* pAdmin = g_playerManager->GetPlayer(i);
-    CBasePlayerController* cPlayer = (CBasePlayerController*)g_pEntitySystem->GetBaseEntity((CEntityIndex)(i + 1));
-
-    if (!cPlayer || !pAdmin || pAdmin->IsFakeClient() || !pAdmin->IsAdminFlagSet(ADMFLAG_SLAY))
-        continue;
-		ClientPrint(cPlayer, HUD_PRINTTALK," \3*************\14Admins Chat\3*************");
-		ClientPrint(cPlayer, HUD_PRINTTALK, " \7[Admins]\4 %s \1from \7%s ", args.ArgS(), player->GetPlayerName());
-		ClientPrint(cPlayer, HUD_PRINTTALK, " \3**************************************");
-}
-}
-*/
 CON_COMMAND_CHAT(u, "admins chat")
 {
     if (!player)
