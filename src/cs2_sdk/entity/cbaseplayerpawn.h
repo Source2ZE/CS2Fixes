17ecb5cf1fd4ca48e22eacce9238109c7b3cdf24
xen <xen@momentum-mod.org>
2023-10-27 15:09:42 +0200

    Use gamedata for CBasePlayerPawn::CommitSuicide

--- src/cs2_sdk/entity/cbaseplayerpawn.h
+++ src/cs2_sdk/entity/cbaseplayerpawn.h
@@ -33,7 +33,11 @@ public:
 	SCHEMA_FIELD(CCSPlayer_ItemServices*, m_pItemServices)
 	SCHEMA_FIELD(CHandle<CBasePlayerController>, m_hController)
 
-	void CommitSuicide(bool bExplode, bool bForce) { CALL_VIRTUAL(void, 354, this, bExplode, bForce); }
+	void CommitSuicide(bool bExplode, bool bForce)
+	{
+		static int offset = g_GameConfig->GetOffset("CBasePlayerPawn_CommitSuicide");
+		CALL_VIRTUAL(void, offset, this, bExplode, bForce);
+	}
 
 	CBasePlayerController *GetController() { return m_hController.Get(); }
 };
