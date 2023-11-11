c2e96c85bd6cbb6c97727ddf0a4e5d0a22b6d386
Poggu <poggu@seznam.cz>
2023-11-11 15:38:13 +0100

    hook say command instead of hooking host_say

--- src/cs2fixes.cpp
+++ src/cs2fixes.cpp
@@ -105,18 +105,9 @@ SH_DECL_HOOK8_void(IGameEventSystem, PostEventAbstract, SH_NOATTRIB, 0, CSplitSc
 SH_DECL_HOOK3_void(INetworkServerService, StartupServer, SH_NOATTRIB, 0, const GameSessionConfiguration_t&, ISource2WorldSession*, const char*);
 SH_DECL_HOOK6_void(ISource2GameEntities, CheckTransmit, SH_NOATTRIB, 0, CCheckTransmitInfo **, int, CBitVec<16384> &, const Entity2Networkable_t **, const uint16 *, int);
 SH_DECL_HOOK2_void(IServerGameClients, ClientCommand, SH_NOATTRIB, 0, CPlayerSlot, const CCommand &);
+SH_DECL_HOOK3_void(ICvar, DispatchConCommand, SH_NOATTRIB, 0, ConCommandHandle, const CCommandContext&, const CCommand&);
 
 CS2Fixes g_CS2Fixes;
-
-#if 0
-// Currently unavailable, requires hl2sdk work!
-ConVar sample_cvar("sample_cvar", "42", 0);
-#endif
-
-CON_COMMAND_F(sample_command, "Sample command", FCVAR_SPONLY | FCVAR_LINKED_CONCOMMAND)
-{
-	Message( "Sample command called by %d. Command: %s\n", context.GetPlayerSlot(), args.GetCommandString() );
-}
 
 CON_COMMAND_F(toggle_logs, "Toggle printing most logs and warnings", FCVAR_SPONLY | FCVAR_LINKED_CONCOMMAND)
 {
@@ -168,6 +159,7 @@ bool CS2Fixes::Load(PluginId id, ISmmAPI *ismm, char *error, size_t maxlen, bool
 	SH_ADD_HOOK_MEMFUNC(IGameEventSystem, PostEventAbstract, g_gameEventSystem, this, &CS2Fixes::Hook_PostEvent, false);
 	SH_ADD_HOOK_MEMFUNC(INetworkServerService, StartupServer, g_pNetworkServerService, this, &CS2Fixes::Hook_StartupServer, true);
 	SH_ADD_HOOK_MEMFUNC(ISource2GameEntities, CheckTransmit, g_pSource2GameEntities, this, &CS2Fixes::Hook_CheckTransmit, true);
+	SH_ADD_HOOK_MEMFUNC(ICvar, DispatchConCommand, g_pCVar, this, &CS2Fixes::Hook_DispatchConCommand, false);
 
 	META_CONPRINTF( "All hooks started!\n" );
 
@@ -270,6 +262,7 @@ bool CS2Fixes::Unload(char *error, size_t maxlen)
 	SH_REMOVE_HOOK_MEMFUNC(IGameEventSystem, PostEventAbstract, g_gameEventSystem, this, &CS2Fixes::Hook_PostEvent, false);
 	SH_REMOVE_HOOK_MEMFUNC(INetworkServerService, StartupServer, g_pNetworkServerService, this, &CS2Fixes::Hook_StartupServer, true);
 	SH_REMOVE_HOOK_MEMFUNC(ISource2GameEntities, CheckTransmit, g_pSource2GameEntities, this, &CS2Fixes::Hook_CheckTransmit, true);
+	SH_REMOVE_HOOK_MEMFUNC(ICvar, DispatchConCommand, g_pCVar, this, &CS2Fixes::Hook_DispatchConCommand, false);
 
 	ConVar_Unregister();
 
@@ -290,6 +283,53 @@ bool CS2Fixes::Unload(char *error, size_t maxlen)
 		delete g_GameConfig;
 
 	return true;
+}
+
+void CS2Fixes::Hook_DispatchConCommand(ConCommandHandle cmdHandle, const CCommandContext& ctx, const CCommand& args)
+{
+	if (!g_pEntitySystem)
+		return;
+
+	auto slot = ctx.GetPlayerSlot();
+
+	bool isSay = V_strcmp(args.Arg(0), "say");
+	bool isTeamSay = V_strcmp(args.Arg(0), "say_team");
+
+	if (slot != -1 && (isSay || isTeamSay))
+	{
+		auto pController = CCSPlayerController::FromSlot(slot);
+		bool bGagged = pController && pController->GetZEPlayer()->IsGagged();
+		bool bFlooding = pController && pController->GetZEPlayer()->IsFlooding();
+
+		if (!bGagged && *args[1] != '/' && !bFlooding)
+		{
+			SH_CALL(g_pCVar, &ICvar::DispatchConCommand)(cmdHandle, ctx, args);
+
+			if (pController)
+			{
+				IGameEvent* pEvent = g_gameEventManager->CreateEvent("player_chat");
+
+				if (pEvent)
+				{
+					pEvent->SetBool("teamonly", isTeamSay);
+					pEvent->SetInt("userid", pController->entindex());
+					pEvent->SetString("text", args[1]);
+
+					g_gameEventManager->FireEvent(pEvent, true);
+				}
+			}
+		}
+		else if (bFlooding)
+		{
+			if (pController)
+				ClientPrint(pController, HUD_PRINTTALK, CHAT_PREFIX "You are flooding the server!");
+		}
+
+		if (*args[1] == '!' || *args[1] == '/')
+			ParseChatCommand(args.ArgS() + 1, pController); // The string returned by ArgS() starts with a \, so skip it
+
+		RETURN_META(MRES_SUPERCEDE);
+	}
 }
 
 void CS2Fixes::Hook_StartupServer(const GameSessionConfiguration_t& config, ISource2WorldSession*, const char*)
