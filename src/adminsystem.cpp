ebb3ab93d93ae8cb38b46f7efbcf4d5e8ed98b42
xen <xen@momentum-mod.org>
2023-11-14 12:30:38 +0200

    Add "cvars" for the various features we have
    Just a ghetto solution until convar support is merged in hl2sdk

--- src/adminsystem.cpp
+++ src/adminsystem.cpp
@@ -900,11 +900,11 @@ CON_COMMAND_CHAT_FLAGS(extend, "extend current map (negative value reduces map d
 
 	int iExtendTime = V_StringToInt32(args[1], 0);
 
-	// CONVAR_TODO
 	ConVar* cvar = g_pCVar->GetConVar(g_pCVar->FindConVar("mp_timelimit"));
 
-	float flTimelimit;
-	memcpy(&flTimelimit, &cvar->values, sizeof(flTimelimit));
+	// CONVAR_TODO
+	// HACK: values is actually the cvar value itself, hence this ugly cast.
+	float flTimelimit = *(float *)cvar->values;
 
 	if (gpGlobals->curtime - g_pGameRules->m_flGameStartTime > flTimelimit * 60)
 		flTimelimit = (gpGlobals->curtime - g_pGameRules->m_flGameStartTime) / 60.0f + iExtendTime;
@@ -918,10 +918,9 @@ CON_COMMAND_CHAT_FLAGS(extend, "extend current map (negative value reduces map d
 	if (flTimelimit <= 0)
 		flTimelimit = 1;
 
+	// CONVAR_TODO
 	char buf[32];
 	V_snprintf(buf, sizeof(buf), "mp_timelimit %.6f", flTimelimit);
-
-	// CONVAR_TODO
 	g_pEngineServer2->ServerCommand(buf);
 
 	if (iExtendTime < 0)
