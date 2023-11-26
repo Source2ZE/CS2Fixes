eedf8c82f08e81e87ab825a497a958efc0405dd6
Poggu <poggu@seznam.cz>
2023-11-26 12:22:02 +0100

    add entity output hook

--- src/detours.h
+++ src/detours.h
@@ -20,6 +20,11 @@
 #pragma once
 #include "cdetour.h"
 
+// variant.h depends on ivscript.h
+DECLARE_POINTER_HANDLE(HSCRIPT);
+
+#include "eiface.h"
+#include <variant.h>
 
 class CCheckTransmitInfo;
 class IRecipientFilter;
@@ -35,6 +40,7 @@ class CGameRules;
 class CTakeDamageInfo;
 class CCSPlayer_WeaponServices;
 class CBasePlayerWeapon;
+class CEntityIOOutput;
 
 bool InitDetours(CGameConfig *gameConfig);
 void FlushAllDetours();
@@ -49,3 +55,4 @@ void FASTCALL Detour_TriggerPush_Touch(CTriggerPush* pPush, Z_CBaseEntity* pOthe
 void FASTCALL Detour_CGameRules_Constructor(CGameRules *pThis);
 void FASTCALL Detour_CBaseEntity_TakeDamageOld(Z_CBaseEntity *pThis, CTakeDamageInfo *inputInfo);
 bool FASTCALL Detour_CCSPlayer_WeaponServices_CanUse(CCSPlayer_WeaponServices *, CBasePlayerWeapon *);
+void FASTCALL Detour_CEntityIOOutput_FireOutputInternal(CEntityIOOutput* const pThis, CEntityInstance* pActivator, CEntityInstance* pCaller, const CVariant* const value, float flDelay);
