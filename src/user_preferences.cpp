68272df3e46270c32dee6c03a9aef9ab0d0cd94a
Envi <112311421+e-n-v-i@users.noreply.github.com>
2024-01-01 13:46:02 +0000

    User preferences REST API implementation.

---
+++ src/user_preferences.cpp
@@ -0,0 +1,246 @@
+/**
+ * =============================================================================
+ * CS2Fixes
+ * Copyright (C) 2023 Source2ZE
+ * =============================================================================
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, version 3.0, as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "commands.h"
+#include "common.h"
+#include "user_preferences.h"
+#include "httpmanager.h"
+#include "playermanager.h"
+#include "strtools.h"
+#include <string>
+#undef snprintf
+#include "vendor/nlohmann/json.hpp"
+
+using json = nlohmann::json;
+
+
+CUserPreferencesSystem* g_pUserPreferencesSystem = nullptr;
+
+
+CON_COMMAND_CHAT_FLAGS(setprefsurl, "Set the preferences API url.", ADMFLAG_ROOT)
+{
+	if (args.ArgC() < 2)
+		Message("Usage: %s <url>. Current value: %s\n", args[0], g_pUserPreferencesSystem->GetPreferencesAPIUrl());
+	else {
+		Message("Setting preferences URL to %s\n", args[1]);
+		g_pUserPreferencesSystem->SetPreferencesAPIUrl(args[1]);
+	}
+}
+
+CON_COMMAND_CHAT_FLAGS(pullprefs, "Pull preferences.", ADMFLAG_ROOT)
+{
+	ZEPlayer* pPlayer = player->GetZEPlayer();
+	if (!pPlayer) return;
+
+	g_pUserPreferencesSystem->PullPreferences(pPlayer->GetPlayerSlot().Get());
+}
+
+CON_COMMAND_CHAT_FLAGS(setpref, "Set a preference key for the player.", ADMFLAG_ROOT)
+{
+	if (args.ArgC() < 3)
+		Message("Usage: %s <pref> <value>\n", args[0]);
+	else {
+		ZEPlayer* pPlayer = player->GetZEPlayer();
+		if (!pPlayer) return;
+
+		g_pUserPreferencesSystem->SetPreference(pPlayer->GetPlayerSlot().Get(), args[1], args[2]);
+	}
+}
+
+CON_COMMAND_CHAT_FLAGS(getpref, "Get a preference key for the player.", ADMFLAG_ROOT)
+{
+	if (args.ArgC() < 2)
+		Message("Usage: %s <pref>\n", args[0]);
+	else {
+		ZEPlayer* pPlayer = player->GetZEPlayer();
+		if (!pPlayer) return;
+
+		const char* sValue = g_pUserPreferencesSystem->GetPreference(pPlayer->GetPlayerSlot().Get(), args[1]);
+		Message("The setting is set to: %s\n", sValue);
+	}
+}
+
+CON_COMMAND_CHAT_FLAGS(pushprefs, "Push preferences.", ADMFLAG_ROOT)
+{
+	ZEPlayer* pPlayer = player->GetZEPlayer();
+	if (!pPlayer) return;
+
+	g_pUserPreferencesSystem->PushPreferences(pPlayer->GetPlayerSlot().Get());
+}
+
+void UserPreferencesHttpCallback(int iSlot, uint64 iSteamID, json data)
+{
+	if (g_pUserPreferencesSystem->PutPreferences(iSlot, iSteamID, data)) {
+		g_pUserPreferencesSystem->OnPutPreferences(iSlot);
+	}
+}
+
+void CUserPreferencesSystem::ClearPreferences(int iSlot) 
+{
+	m_mUserSteamIds[iSlot] = 0;
+	m_mPreferencesLoaded[iSlot] = false;
+	m_mPreferencesMaps[iSlot].Purge();
+}
+
+bool CUserPreferencesSystem::PutPreferences(int iSlot, uint64 iSteamID, json preferenceData)
+{
+	ZEPlayer* player = g_playerManager->GetPlayer(CPlayerSlot(iSlot));
+	if (!player || !player->IsAuthenticated()) return false;
+	if (iSteamID != player->GetSteamId64()) {
+		return false;
+	}
+#ifdef _DEBUG
+	Message("Putting data for %llu\n", iSteamID);
+#endif
+	m_mUserSteamIds[iSlot] = iSteamID;
+	m_mPreferencesLoaded[iSlot] = true;
+	for (auto it = preferenceData.begin(); it != preferenceData.end(); ++it) {
+		std::string key = it.key();
+		std::string value = it.value();
+
+		// Prepare the key and value pair, and also the key name via hashes
+#ifdef _DEBUG
+		Message("- Storing KV-pair: %s, %s\n", key.c_str(), value.c_str());
+#endif
+		CPreferenceValue* prefValue = new CPreferenceValue(key.c_str(), value.c_str());
+
+		// Store key, value, and key names
+		uint32 iKeyHash = hash_32_fnv1a_const(prefValue->GetKey());
+		m_mPreferencesMaps[iSlot].InsertOrReplace(iKeyHash, *prefValue);
+	}
+	return true;
+} 
+
+void CUserPreferencesSystem::OnPutPreferences(int iSlot)
+{
+	int iHideDistance = GetPreferenceInt(iSlot, HIDE_DISTANCE_PREF_KEY_NAME, 0);
+	int iSoundStatus = GetPreferenceInt(iSlot, SOUND_STATUS_PREF_KEY_NAME, 2);
+	bool bStopSound = (bool) (iSoundStatus & 1);
+	bool bSilenceSound = (bool) (iSoundStatus & 2);
+	bool bHideDecals = (bool) GetPreferenceInt(iSlot, DECAL_PREF_KEY_NAME, 1);
+
+	// Set the values that we just loaded --- the player is guaranteed available
+	g_playerManager->SetPlayerStopSound(iSlot, bStopSound);
+	g_playerManager->SetPlayerSilenceSound(iSlot, bSilenceSound);
+	g_playerManager->SetPlayerStopDecals(iSlot, bHideDecals);
+
+	ZEPlayer* player = g_playerManager->GetPlayer(CPlayerSlot(iSlot));
+	player->SetHideDistance(iHideDistance);
+}
+
+void CUserPreferencesSystem::PullPreferences(int iSlot) 
+{
+	if (m_pszUserPreferencesUrl[0] == '\0') return;
+
+	// Ignore non-authenticated players and get the authenticated SteamID
+	ZEPlayer* player = g_playerManager->GetPlayer(CPlayerSlot(iSlot));
+	if (!player || !player->IsAuthenticated()) return;
+	uint64 iSteamId = player->GetSteamId64();
+
+	// Submit the request to pull the user data
+	char sUserPreferencesUrl[256];
+	V_snprintf(sUserPreferencesUrl, sizeof(sUserPreferencesUrl), "%s%llu", m_pszUserPreferencesUrl, iSteamId);
+	g_HTTPManager.GET(sUserPreferencesUrl, [iSlot, iSteamId](HTTPRequestHandle request, json data){
+		UserPreferencesHttpCallback(iSlot, iSteamId, data);
+	});
+}
+
+const char* CUserPreferencesSystem::GetPreference(int iSlot, const char* sKey, const char* sDefaultValue)
+{
+	uint32 iKeyHash = hash_32_fnv1a_const(sKey);
+	int iKeyIdx = m_mPreferencesMaps[iSlot].Find(iKeyHash);
+#ifdef _DEBUG
+	Message("User at slot %d is reading from preference '%s' with hash %d at index %d.\n", iSlot, sKey, iKeyHash, iKeyIdx);
+#endif
+	if (iKeyIdx == m_mPreferencesMaps[iSlot].InvalidIndex()) return sDefaultValue;
+	return (const char*) m_mPreferencesMaps[iSlot][iKeyIdx].GetValue();
+}
+
+int CUserPreferencesSystem::GetPreferenceInt(int iSlot, const char* sKey, int iDefaultValue)
+{
+	return V_StringToInt32(GetPreference(iSlot, sKey), iDefaultValue);
+}
+
+float CUserPreferencesSystem::GetPreferenceFloat(int iSlot, const char* sKey, float fDefaultValue)
+{
+	return V_StringToFloat32(GetPreference(iSlot, sKey), fDefaultValue);
+}
+
+void CUserPreferencesSystem::SetPreference(int iSlot, const char* sKey, const char* sValue)
+{
+	uint32 iKeyHash = hash_32_fnv1a_const(sKey);
+#ifdef _DEBUG
+	Message("User at slot %d is storing in preference '%s' with hash %d value '%s'.\n", iSlot, sKey, iKeyHash, sValue);
+#endif
+
+	// Create or populate the content of the preference value
+	int iValueIdx = m_mPreferencesMaps[iSlot].Find(iKeyHash);
+	CPreferenceValue* prefValue;
+	if (iValueIdx == m_mPreferencesMaps[iSlot].InvalidIndex()) {
+		prefValue = new CPreferenceValue(sKey, sValue);
+	} else {
+		prefValue = &m_mPreferencesMaps[iSlot][iValueIdx];
+		prefValue->SetKeyValue(sKey, sValue);
+	}
+
+    // Override the key-value pair and insert
+	m_mPreferencesMaps[iSlot].InsertOrReplace(iKeyHash, *prefValue);
+}
+
+void CUserPreferencesSystem::SetPreferenceInt(int iSlot, const char* sKey, int iValue)
+{
+	char sPreferenceString[MAX_PREFERENCE_LENGTH];
+	V_snprintf(sPreferenceString, sizeof(sPreferenceString), "%d", iValue);
+	SetPreference(iSlot, sKey, (const char*) sPreferenceString);
+}
+
+void CUserPreferencesSystem::SetPreferenceFloat(int iSlot, const char* sKey, float fValue)
+{
+	char sPreferenceString[MAX_PREFERENCE_LENGTH];
+	V_snprintf(sPreferenceString, sizeof(sPreferenceString), "%f", fValue);
+	SetPreference(iSlot, sKey, (const char*) sPreferenceString);
+}
+
+void CUserPreferencesSystem::PushPreferences(int iSlot)
+{
+	// Fetch the slot and only 'push' if the player has already loaded
+	if (!m_mPreferencesLoaded[iSlot]) return;
+	uint64 iSteamId = m_mUserSteamIds[iSlot];
+
+	// Create the JSON object with all key-value pairs
+	json sJsonObject = json::object();
+	FOR_EACH_MAP(m_mPreferencesMaps[iSlot], i) {
+		uint32 iKeyHash = m_mPreferencesMaps[iSlot].Key(i);
+		int iValueIdx = m_mPreferencesMaps[iSlot].Find(iKeyHash);
+		if (iValueIdx == m_mPreferencesMaps[iSlot].InvalidIndex())
+			continue;
+		CPreferenceValue prefValue = m_mPreferencesMaps[iSlot][iValueIdx];
+		sJsonObject[prefValue.GetKey()] = prefValue.GetValue();
+	}
+
+	// Prepare the API URL to send the request to
+	char sUserPreferencesUrl[256];
+	V_snprintf(sUserPreferencesUrl, sizeof(sUserPreferencesUrl), "%s%llu", m_pszUserPreferencesUrl, iSteamId);
+	
+	// Dump the Json object and submit the POST request
+	const char* sDumpedJson = sJsonObject.dump().c_str();
+	g_HTTPManager.POST(sUserPreferencesUrl, sDumpedJson, [iSlot, iSteamId](HTTPRequestHandle request, json data){
+		UserPreferencesHttpCallback(iSlot, iSteamId, data);
+	});
+}
---
+++ src/user_preferences.h
@@ -0,0 +1,83 @@
+/**
+ * =============================================================================
+ * CS2Fixes
+ * Copyright (C) 2023 Source2ZE
+ * =============================================================================
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, version 3.0, as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include "utlmap.h"
+#include "utlstring.h"
+#undef snprintf
+#include "vendor/nlohmann/json.hpp"
+
+using json = nlohmann::json;
+
+#define MAX_PREFERENCE_LENGTH 128
+
+
+class CPreferenceValue
+{
+public:
+	CPreferenceValue(const char* sKey = "", const char* sValue = "")
+	{
+		V_strcpy(m_sKey, sKey);
+		V_strcpy(m_sValue, sValue);
+	}
+	char* GetKey() { return m_sKey; };
+	char* GetValue() { return m_sValue; };
+	void SetKeyValue(const char* sKey, const char* sValue) {
+		V_strcpy(m_sKey, sKey);
+		V_strcpy(m_sValue, sValue);
+	}
+
+private:
+	char m_sKey[MAX_PREFERENCE_LENGTH];
+	char m_sValue[MAX_PREFERENCE_LENGTH];
+};
+
+
+class CUserPreferencesSystem
+{
+public:
+	CUserPreferencesSystem()
+	{
+		for (int i = 0; i < MAXPLAYERS; i++) {
+			m_mPreferencesMaps[i].SetLessFunc(DefLessFunc(uint32));
+			m_mPreferencesLoaded[i] = false;
+		}
+	}
+
+	void ClearPreferences(int iSlot);
+	void PullPreferences(int iSlot);
+	const char* GetPreference(int iSlot, const char* sKey, const char* sDefaultValue = "");
+	int GetPreferenceInt(int iSlot, const char* sKey, int iDefaultValue = 0);
+	float GetPreferenceFloat(int iSlot, const char* sKey, float fDefaultValue = 0.0f);
+	void SetPreference(int iSlot, const char* sKey, const char* sValue);
+	void SetPreferenceInt(int iSlot, const char* sKey, int iValue);
+	void SetPreferenceFloat(int iSlot, const char* sKey, float fValue);
+	bool PutPreferences(int iSlot, uint64 iSteamId, json preferenceData);
+	void OnPutPreferences(int iSlot);
+	void PushPreferences(int iSlot);
+	void SetPreferencesAPIUrl(const char* sUserPreferencesUrl) { V_strcpy(m_pszUserPreferencesUrl, sUserPreferencesUrl); };
+	const char* GetPreferencesAPIUrl() { return (const char*) m_pszUserPreferencesUrl; };
+private:
+	char m_pszUserPreferencesUrl[256] = "";
+	CUtlMap<uint32, CPreferenceValue> m_mPreferencesMaps[MAXPLAYERS];
+	uint64 m_mUserSteamIds[MAXPLAYERS];
+	bool m_mPreferencesLoaded[MAXPLAYERS];
+};
+
+extern CUserPreferencesSystem* g_pUserPreferencesSystem;
