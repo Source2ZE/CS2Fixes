24102cf97d6a56822f07c60a28470a2e96b7bc68
Vauff <mctehkitti@gmail.com>
2024-01-07 12:52:11 -0500

    Fix MZ infection using invalid spawns & fix classic spawn

--- src/cs2_sdk/entity/cgamerules.h
+++ src/cs2_sdk/entity/cgamerules.h
@@ -67,6 +67,8 @@ public:
 	SCHEMA_FIELD(GameTime_t, m_flRestartRoundTime)
 	SCHEMA_FIELD_POINTER(int, m_nEndMatchMapGroupVoteOptions)
 	SCHEMA_FIELD(int, m_nEndMatchMapVoteWinner)
+	SCHEMA_FIELD_POINTER(CUtlVector<SpawnPoint*>, m_CTSpawnPoints)
+	SCHEMA_FIELD_POINTER(CUtlVector<SpawnPoint*>, m_TerroristSpawnPoints)
 
 	void TerminateRound(float flDelay, CSRoundEndReason reason)
 	{
