6b1aebca3fe17d17d2daa0c4bbad72b6376f7ac5
xen <xen@momentum-mod.org>
2024-02-07 08:05:40 +0200

    Update keyvalue usage to reflect sdk changes

--- src/gameconfig.cpp
+++ src/gameconfig.cpp
@@ -21,7 +21,7 @@ bool CGameConfig::Init(IFileSystem *filesystem, char *conf_error, int conf_error
 		return false;
 	}
 
-	const KeyValues* game = m_pKeyValues->FindKey(m_szGameDir.c_str());
+	KeyValues* game = m_pKeyValues->FindKey(m_szGameDir.c_str(), false);
 	if (game)
 	{
 #if defined _LINUX
@@ -30,7 +30,7 @@ bool CGameConfig::Init(IFileSystem *filesystem, char *conf_error, int conf_error
 		const char* platform = "windows";
 #endif
 
-		const KeyValues* offsets = game->FindKey("Offsets");
+		KeyValues* offsets = game->FindKey("Offsets", false);
 		if (offsets)
 		{
 			FOR_EACH_SUBKEY(offsets, it)
@@ -39,7 +39,7 @@ bool CGameConfig::Init(IFileSystem *filesystem, char *conf_error, int conf_error
 			}
 		}
 
-		const KeyValues* signatures = game->FindKey("Signatures");
+		KeyValues *signatures = game->FindKey("Signatures", false);
 		if (signatures)
 		{
 			FOR_EACH_SUBKEY(signatures, it)
@@ -49,7 +49,7 @@ bool CGameConfig::Init(IFileSystem *filesystem, char *conf_error, int conf_error
 			}
 		}
 
-		const KeyValues* patches = game->FindKey("Patches");
+		KeyValues *patches = game->FindKey("Patches", false);
 		if (patches)
 		{
 			FOR_EACH_SUBKEY(patches, it)
