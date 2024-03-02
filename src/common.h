e5c47767bb35a392433be890fcd97d5a380d1ce7
xen <xen@momentum-mod.org>
2024-03-02 15:59:50 +0200

    Add flashlight brightness and color cvars

--- src/common.h
+++ src/common.h
@@ -78,3 +78,14 @@ void Panic(const char *, ...);
 		else																											\
 			variable_name = args[1];																					\
 	}
+
+#define FAKE_COLOR_CVAR(name, description, variable_name, protect)														\
+	CON_COMMAND_F(name, description, FCVAR_LINKED_CONCOMMAND | FCVAR_SPONLY | (protect ? FCVAR_PROTECTED : FCVAR_NONE))	\
+	{																													\
+		if (args.ArgC() < 2)																							\
+			Msg("%s %i %i %i\n", args[0], variable_name[0], variable_name[1], variable_name[2]);						\
+		else if (args.ArgC() == 2)																						\
+			V_StringToColor(args[1], variable_name);																	\
+		else																											\
+			V_StringToColor(args.ArgS(), variable_name);																\
+	}
