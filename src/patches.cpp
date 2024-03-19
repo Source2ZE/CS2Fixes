6f07918d8ab997cccf0e321adcd7a298dc34481f
xen <xen@momentum-mod.org>
2024-03-19 20:39:29 +0200

    Add patch to fix caller in CPhysBox::Use

--- src/patches.cpp
+++ src/patches.cpp
@@ -38,6 +38,7 @@ CMemPatch g_CommonPatches[] =
 	CMemPatch("CheckJumpButtonWater", "FixWaterFloorJump"),
 	CMemPatch("CategorizeUnderwater", "CategorizeUnderwater"),
 	CMemPatch("WaterLevelGravity", "WaterLevelGravity"),
+	CMemPatch("CPhysBox_Use", "CPhysBox_Use"),
 	CMemPatch("BotNavIgnore", "BotNavIgnore"),
 #ifndef _WIN32
 	// Linux checks for the nav mesh in each bot_add command, so we patch 3 times
