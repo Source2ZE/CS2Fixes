06bc1e146a3bd2c071453cd7d5aa1e9ef0bef40f
Poggu <poggu@seznam.cz>
2024-03-25 19:33:17 +0100

    add vtable pattern scanning based on dynlibutils
    https://github.com/komashchenko/DynLibUtils/

--- src/utils/module.h
+++ src/utils/module.h
@@ -24,6 +24,10 @@
 #include "strtools.h"
 #include "plat.h"
 
+#include <string>
+#include <vector>
+
+
 #ifdef _WIN32
 #include <Psapi.h>
 #endif
@@ -33,6 +37,42 @@ enum SigError
 	SIG_OK,
 	SIG_NOT_FOUND,
 	SIG_FOUND_MULTIPLE,
+};
+
+// equivalent to FindSignature, but allows for multiple signatures to be found and iterated over
+class SignatureIterator
+{
+public:
+	SignatureIterator(void* pBase, size_t iSize, const byte* pSignature, size_t iSigLength) :
+		m_pBase((byte*)pBase), m_iSize(iSize), m_pSignature(pSignature), m_iSigLength(iSigLength)
+	{
+		m_pCurrent = m_pBase;
+	}
+
+	void* FindNext(bool allowWildcard)
+	{
+		for (size_t i = 0; i < m_iSize; i++)
+		{
+			size_t Matches = 0;
+			while (*(m_pCurrent + i + Matches) == m_pSignature[Matches] || (allowWildcard && m_pSignature[Matches] == '\x2A'))
+			{
+				Matches++;
+				if (Matches == m_iSigLength)
+				{
+					m_pCurrent += i + 1;
+					return m_pCurrent - 1;
+				}
+			}
+		}
+
+		return nullptr;
+	}
+private:
+	byte* m_pBase;
+	size_t m_iSize;
+	const byte* m_pSignature;
+	size_t m_iSigLength;
+	byte* m_pCurrent;
 };
 
 class CModule
@@ -56,10 +96,14 @@ public:
 
 		m_base = (void *)m_hModuleInfo.lpBaseOfDll;
 		m_size = m_hModuleInfo.SizeOfImage;
+		InitializeSections();
 #else
-		if (int e = GetModuleInformation(m_hModule, &m_base, &m_size))
+		if (int e = GetModuleInformation(m_hModule, &m_base, &m_size, m_sections))
 			Error("Failed to get module info for %s, error %d\n", szModule, e);
 #endif
+
+		for(auto& section : m_sections)
+			Message("Section %s base: 0x%p | size: %d\n", section.m_szName.c_str(), section.m_pBase, section.m_iSize);
 
 		Message("Initialized module %s base: 0x%p | size: %d\n", m_pszModule, m_base, m_size);
 	}
@@ -115,9 +159,25 @@ public:
 		return pInterface;
 	}
 
+	Section* GetSection(const std::string_view name)
+	{
+		for (auto& section : m_sections)
+		{
+			if (section.m_szName == name)
+				return &section;
+		}
+
+		return nullptr;
+	}
+#ifdef _WIN32
+	void InitializeSections();
+#endif
+	void* FindVirtualTable(const std::string& name);
+public:
 	const char *m_pszModule;
 	const char* m_pszPath;
 	HINSTANCE m_hModule;
 	void* m_base;
 	size_t m_size;
+	std::vector<Section> m_sections;
 };
