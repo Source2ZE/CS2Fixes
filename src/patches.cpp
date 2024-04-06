f384b0b948dcfd70437ec94df5a1355e33ce95f8
xen <xen@momentum-mod.org>
2024-04-06 20:07:04 +0200

    Get rid of vscript
    yeet

--- src/patches.cpp
+++ src/patches.cpp
@@ -34,7 +34,6 @@ extern CGameConfig *g_GameConfig;
 CMemPatch g_CommonPatches[] =
 {
 	CMemPatch("ServerMovementUnlock", "ServerMovementUnlock"),
-	CMemPatch("VScriptEnable", "VScriptEnable"),
 	CMemPatch("CheckJumpButtonWater", "FixWaterFloorJump"),
 	CMemPatch("CategorizeUnderwater", "CategorizeUnderwater"),
 	CMemPatch("WaterLevelGravity", "WaterLevelGravity"),
