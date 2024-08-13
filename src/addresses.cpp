92f1afbdf222f798983b137e9a632d3e70d595e6
xen <xen@momentum-mod.org>
2024-08-13 11:02:57 +0300

    Get rid of ClientPrint signatures

--- src/addresses.cpp
+++ src/addresses.cpp
@@ -50,8 +50,6 @@ bool addresses::Initialize(CGameConfig *g_GameConfig)
 		modules::hammer = new CModule(ROOTBIN, "tools/hammer");
 #endif
 
-	RESOLVE_SIG(g_GameConfig, "UTIL_ClientPrintAll", addresses::UTIL_ClientPrintAll);
-	RESOLVE_SIG(g_GameConfig, "ClientPrint", addresses::ClientPrint);
 	RESOLVE_SIG(g_GameConfig, "SetGroundEntity", addresses::SetGroundEntity);
 	RESOLVE_SIG(g_GameConfig, "CCSPlayerController_SwitchTeam", addresses::CCSPlayerController_SwitchTeam);
 	RESOLVE_SIG(g_GameConfig, "CBasePlayerController_SetPawn", addresses::CBasePlayerController_SetPawn);
