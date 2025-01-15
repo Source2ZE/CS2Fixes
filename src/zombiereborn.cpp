Source2ZE/CS2Fixes

commit e4793ce76425c938b9d9e5aaf38de244af1fe073

Use bDontBroadcast in fake infect death events (#333)
And add death event to mother zombie infections.

src/zombiereborn.cpp
@@ -1118,7 +1118,7 @@ void ZR_ApplyKnockbackExplosion(CBaseEntity* pProjectile, CCSPlayerPawn* pVictim
 	pVictim->m_vecAbsVelocity = pVictim->m_vecAbsVelocity() + vecKnockback;
 }
 
-void ZR_FakePlayerDeath(CCSPlayerController* pAttackerController, CCSPlayerController* pVictimController, const char* szWeapon)
+void ZR_FakePlayerDeath(CCSPlayerController* pAttackerController, CCSPlayerController* pVictimController, const char* szWeapon, bool bDontBroadcast)
 {
 	IGameEvent* pEvent = g_gameEventManager->CreateEvent("player_death");
 
@@ -1132,7 +1132,7 @@ void ZR_FakePlayerDeath(CCSPlayerController* pAttackerController, CCSPlayerContr
 	pEvent->SetString("weapon", szWeapon);
 	pEvent->SetBool("infected", true);
 
-	g_gameEventManager->FireEvent(pEvent, false);
+	g_gameEventManager->FireEvent(pEvent, bDontBroadcast);
 }
 
 void ZR_StripAndGiveKnife(CCSPlayerPawn* pPawn)
@@ -1265,8 +1265,7 @@ void ZR_Infect(CCSPlayerController* pAttackerController, CCSPlayerController* pV
 
 	ZR_CheckTeamWinConditions(CS_TEAM_T);
 
-	if (!bDontBroadcast)
-		ZR_FakePlayerDeath(pAttackerController, pVictimController, "knife"); // or any other killicon
+	ZR_FakePlayerDeath(pAttackerController, pVictimController, "knife", bDontBroadcast); // or any other killicon
 
 	CCSPlayerPawn* pVictimPawn = (CCSPlayerPawn*)pVictimController->GetPawn();
 	if (!pVictimPawn)
@@ -1301,6 +1300,9 @@ void ZR_InfectMotherZombie(CCSPlayerController* pVictimController, std::vector<S
 		return;
 
 	pVictimController->SwitchTeam(CS_TEAM_T);
+
+	ZR_FakePlayerDeath(pVictimController, pVictimController, "knife", true); // not sent to clients
+
 	ZR_StripAndGiveKnife(pVictimPawn);
 
 	// pick random spawn point

commit b04de82fb97c318ce943f85ed8df1f90adab4a7a

Don't create infection death event for players already dead
Fixes a crash with bots https://github.com/Source2ZE/CS2Fixes/commit/e4793ce76425c938b9d9e5aaf38de244af1fe073 introduced, but this just generally makes sense to check anyways

src/zombiereborn.cpp
@@ -1120,6 +1120,9 @@ void ZR_ApplyKnockbackExplosion(CBaseEntity* pProjectile, CCSPlayerPawn* pVictim
 
 void ZR_FakePlayerDeath(CCSPlayerController* pAttackerController, CCSPlayerController* pVictimController, const char* szWeapon, bool bDontBroadcast)
 {
+	if (!pVictimController->m_bPawnIsAlive())
+		return;
+
 	IGameEvent* pEvent = g_gameEventManager->CreateEvent("player_death");
 
 	if (!pEvent)