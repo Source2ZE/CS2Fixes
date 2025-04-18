754e22aec84c5831a4f95476d6f31246d2cfc92c
Vauff <mctehkitti@gmail.com>
2025-04-17 22:44:40 -0400

    Fix movement unlocker toggle regression & remove some old patches

--- src/patches.cpp
+++ src/patches.cpp
@@ -45,24 +45,6 @@ CMemPatch g_CommonPatches[] =
 #endif
 };
 
-CMemPatch g_ClientPatches[] =
-	{
-		CMemPatch("ClientMovementUnlock", "ClientMovementUnlock"),
-};
-
-#ifdef _WIN32
-CMemPatch g_ToolsPatches[] =
-	{
-		// Remove some -nocustomermachine checks without needing -nocustomermachine itself
-		// as it can break stuff like the asset browser. This is mainly to enable device selection in compiles
-		// And yes, it's the exact same signature appearing 4 times, each unhides a certain hammer compile option
-		CMemPatch("HammerNoCustomerMachine", "HammerNoCustomerMachine"),
-		CMemPatch("HammerNoCustomerMachine", "HammerNoCustomerMachine"),
-		CMemPatch("HammerNoCustomerMachine", "HammerNoCustomerMachine"),
-		CMemPatch("HammerNoCustomerMachine", "HammerNoCustomerMachine"),
-};
-#endif
-
 CConVar<bool> cs2f_movement_unlocker_enable("cs2f_movement_unlocker_enable", FCVAR_NONE, "Whether to enable movement unlocker", false,
 											[](CConVar<bool>* cvar, CSplitScreenSlot slot, const bool* new_val, const bool* old_val) {
 												// Movement unlocker is always the first patch
@@ -75,24 +57,12 @@ CConVar<bool> cs2f_movement_unlocker_enable("cs2f_movement_unlocker_enable", FCV
 bool InitPatches(CGameConfig* g_GameConfig)
 {
 	bool success = true;
-	for (int i = 0; i < sizeof(g_CommonPatches) / sizeof(*g_CommonPatches); i++)
+
+	// Skip first patch (movement unlocker), it gets patched in convar callback
+	for (int i = 1; i < sizeof(g_CommonPatches) / sizeof(*g_CommonPatches); i++)
 		if (!g_CommonPatches[i].PerformPatch(g_GameConfig))
 			success = false;
 
-	// Dedicated servers don't load client
-	if (!CommandLine()->HasParm("-dedicated"))
-	{
-		for (int i = 0; i < sizeof(g_ClientPatches) / sizeof(*g_ClientPatches); i++)
-			if (!g_ClientPatches[i].PerformPatch(g_GameConfig))
-				success = false;
-	}
-
-#ifdef _WIN32
-	// None of the tools are loaded without, well, -tools
-	if (CommandLine()->HasParm("-tools"))
-		for (int i = 0; i < sizeof(g_ToolsPatches) / sizeof(*g_ToolsPatches); i++)
-			g_ToolsPatches[i].PerformPatch(g_GameConfig);
-#endif
 	return success;
 }
 
@@ -100,14 +70,4 @@ void UndoPatches()
 {
 	for (int i = 0; i < sizeof(g_CommonPatches) / sizeof(*g_CommonPatches); i++)
 		g_CommonPatches[i].UndoPatch();
-
-	if (!CommandLine()->HasParm("-dedicated"))
-		for (int i = 0; i < sizeof(g_ClientPatches) / sizeof(*g_ClientPatches); i++)
-			g_ClientPatches[i].UndoPatch();
-
-#ifdef _WIN32
-	if (CommandLine()->HasParm("-tools"))
-		for (int i = 0; i < sizeof(g_ToolsPatches) / sizeof(*g_ToolsPatches); i++)
-			g_ToolsPatches[i].UndoPatch();
-#endif
 }
