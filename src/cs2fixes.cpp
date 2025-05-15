60d44203a8c45eca36f1b0747b578f5625e42f87
Vauff <mctehkitti@gmail.com>
2025-05-15 02:36:09 -0400

    Add more weapon sounds to !stopsound

--- src/cs2fixes.cpp
+++ src/cs2fixes.cpp
@@ -649,6 +649,11 @@ void CS2Fixes::Hook_GameServerSteamAPIDeactivated()
 	RETURN_META(MRES_IGNORED);
 }
 
+uint32 GetSoundEventHash(const char* pszSoundEventName)
+{
+	return MurmurHash2LowerCase(pszSoundEventName, 0x53524332);
+}
+
 void CS2Fixes::Hook_PostEvent(CSplitScreenSlot nSlot, bool bLocalOnly, int nClientCount, const uint64* clients,
 							  INetworkMessageInternal* pEvent, const CNetMessage* pData, unsigned long nSize, NetChannelBufType_t bufType)
 {
@@ -712,13 +717,63 @@ void CS2Fixes::Hook_PostEvent(CSplitScreenSlot nSlot, bool bLocalOnly, int nClie
 	}
 	else if (g_cvarEnableStopSound.Get() && info->m_MessageId == GE_SosStartSoundEvent)
 	{
+		static std::set<uint32> soundEventHashes;
 		auto msg = const_cast<CNetMessage*>(pData)->ToPB<CMsgSosStartSoundEvent>();
 
-		if (msg->soundevent_hash() == MurmurHash2LowerCase("Weapon_Revolver.Prepare", 0x53524332))
+		ExecuteOnce(
+			soundEventHashes.insert(GetSoundEventHash("Weapon_Knife.HitWall"));
+			soundEventHashes.insert(GetSoundEventHash("Weapon_Knife.Slash"));
+			soundEventHashes.insert(GetSoundEventHash("Weapon_Knife.Hit"));
+			soundEventHashes.insert(GetSoundEventHash("Weapon_Knife.Stab"));
+			soundEventHashes.insert(GetSoundEventHash("Weapon_sg556.ZoomIn"));
+			soundEventHashes.insert(GetSoundEventHash("Weapon_sg556.ZoomOut"));
+			soundEventHashes.insert(GetSoundEventHash("Weapon_AUG.ZoomIn"));
+			soundEventHashes.insert(GetSoundEventHash("Weapon_AUG.ZoomOut"));
+			soundEventHashes.insert(GetSoundEventHash("Weapon_SSG08.Zoom"));
+			soundEventHashes.insert(GetSoundEventHash("Weapon_SSG08.ZoomOut"));
+			soundEventHashes.insert(GetSoundEventHash("Weapon_SCAR20.Zoom"));
+			soundEventHashes.insert(GetSoundEventHash("Weapon_SCAR20.ZoomOut"));
+			soundEventHashes.insert(GetSoundEventHash("Weapon_G3SG1.Zoom"));
+			soundEventHashes.insert(GetSoundEventHash("Weapon_G3SG1.ZoomOut"));
+			soundEventHashes.insert(GetSoundEventHash("Weapon_AWP.Zoom"));
+			soundEventHashes.insert(GetSoundEventHash("Weapon_AWP.ZoomOut"));
+			soundEventHashes.insert(GetSoundEventHash("Weapon_Revolver.Prepare"));
+		);
+
+		if (soundEventHashes.contains(msg->soundevent_hash()))
 		{
-			// Filter out people using stop/silence sound from hearing R8 windup
-			*(uint64*)clients &= ~g_playerManager->GetStopSoundMask();
-			*(uint64*)clients &= ~g_playerManager->GetSilenceSoundMask();
+			uint64 stopSoundMask = g_playerManager->GetStopSoundMask();
+			uint64 silenceSoundMask = g_playerManager->GetSilenceSoundMask();
+
+			if (msg->has_source_entity_index())
+			{
+				CBaseEntity* pSourceEntity = (CBaseEntity*)g_pEntitySystem->GetEntityInstance(CEntityIndex(msg->source_entity_index()));
+				int playerSlot = -1;
+
+				if (!V_strcasecmp(pSourceEntity->GetClassname(), "player"))
+				{
+					playerSlot = ((CCSPlayerPawn*)pSourceEntity)->GetController()->GetPlayerSlot();
+				}
+				else if (!V_strncasecmp(pSourceEntity->GetClassname(), "weapon_", 7))
+				{
+					CCSPlayerPawn* pPawn = (CCSPlayerPawn*)pSourceEntity->m_hOwnerEntity().Get();
+
+					if (pPawn && pPawn->IsPawn())
+						playerSlot = pPawn->GetController()->GetPlayerSlot();
+				}
+
+				// Remove player who triggered this sound from masks
+				// Because some of these sounds never get played locally (Zoom's, Knife Hit/Stab)
+				if (playerSlot != -1 && g_playerManager->IsPlayerUsingStopSound(playerSlot))
+					stopSoundMask &= ~((uint64)1 << playerSlot);
+
+				if (playerSlot != -1 && g_playerManager->IsPlayerUsingSilenceSound(playerSlot))
+					silenceSoundMask &= ~((uint64)1 << playerSlot);
+			}
+
+			// Filter out people using stop/silence sound from hearing this sound from other players
+			*(uint64*)clients &= ~stopSoundMask;
+			*(uint64*)clients &= ~silenceSoundMask;
 		}
 	}
 }
