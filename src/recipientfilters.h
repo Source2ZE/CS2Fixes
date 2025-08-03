57829f30226ae8c30a6c8cb52555e381ab9c2b87
Vauff <mctehkitti@gmail.com>
2025-08-03 11:47:40 -0400

    Hacky fixes for recipient filters

--- src/recipientfilters.h
+++ src/recipientfilters.h
@@ -21,63 +21,31 @@
 #include "irecipientfilter.h"
 #include "playermanager.h"
 
-// Simple filter for when only 1 recipient is needed
-class CSingleRecipientFilter : public IRecipientFilter
-{
-public:
-	CSingleRecipientFilter(CPlayerSlot iRecipient, NetChannelBufType_t nBufType = BUF_RELIABLE, bool bInitMessage = false) :
-		m_iRecipient(iRecipient), m_nBufType(nBufType), m_bInitMessage(bInitMessage) {}
-
-	~CSingleRecipientFilter() override {}
-
-	NetChannelBufType_t GetNetworkBufType(void) const override { return m_nBufType; }
-	bool IsInitMessage(void) const override { return m_bInitMessage; }
-	int GetRecipientCount(void) const override { return 1; }
-	CPlayerSlot GetRecipientIndex(int slot) const override { return m_iRecipient; }
-
-private:
-	CPlayerSlot m_iRecipient;
-	NetChannelBufType_t m_nBufType;
-	bool m_bInitMessage;
-};
-
 class CRecipientFilter : public IRecipientFilter
 {
 public:
-	CRecipientFilter()
-	{
-		m_nBufType = BUF_RELIABLE;
-		m_bInitMessage = false;
-	}
+	CRecipientFilter(NetChannelBufType_t nBufType = BUF_RELIABLE, bool bInitMessage = false) :
+		m_nBufType(nBufType), m_bInitMessage(bInitMessage) {}
 
-	CRecipientFilter(IRecipientFilter* source, int iExcept = -1)
+	CRecipientFilter(IRecipientFilter* source, CPlayerSlot exceptSlot = -1)
 	{
+		m_Recipients = source->GetRecipients();
 		m_nBufType = source->GetNetworkBufType();
 		m_bInitMessage = source->IsInitMessage();
-		m_Recipients.RemoveAll();
 
-		for (int i = 0; i < source->GetRecipientCount(); i++)
-			if (source->GetRecipientIndex(i).Get() != iExcept)
-				m_Recipients.AddToTail(source->GetRecipientIndex(i));
+		if (exceptSlot != -1)
+			m_Recipients.Clear(exceptSlot.Get());
 	}
 
 	~CRecipientFilter() override {}
 
 	NetChannelBufType_t GetNetworkBufType(void) const override { return m_nBufType; }
 	bool IsInitMessage(void) const override { return m_bInitMessage; }
-	int GetRecipientCount(void) const override { return m_Recipients.Count(); }
-
-	CPlayerSlot GetRecipientIndex(int slot) const override
-	{
-		if (slot < 0 || slot >= GetRecipientCount())
-			return CPlayerSlot(-1);
-
-		return m_Recipients[slot];
-	}
+	const CPlayerBitVec& GetRecipients(void) const override { return m_Recipients; }
 
 	void AddAllPlayers(void)
 	{
-		m_Recipients.RemoveAll();
+		m_Recipients.ClearAll();
 
 		for (int i = 0; i < MAXPLAYERS; i++)
 		{
@@ -90,15 +58,24 @@ public:
 
 	void AddRecipient(CPlayerSlot slot)
 	{
-		// Don't add if it already exists
-		if (m_Recipients.Find(slot) != m_Recipients.InvalidIndex())
-			return;
-
-		m_Recipients.AddToTail(slot);
+		if (slot.Get() >= 0 && slot.Get() < ABSOLUTE_PLAYER_LIMIT)
+			m_Recipients.Set(slot.Get());
 	}
 
-private:
+protected:
 	NetChannelBufType_t m_nBufType;
 	bool m_bInitMessage;
-	CUtlVectorFixed<CPlayerSlot, MAXPLAYERS> m_Recipients;
+	CPlayerBitVec m_Recipients;
+};
+
+// Simple filter for when only 1 recipient is needed
+class CSingleRecipientFilter : public CRecipientFilter
+{
+public:
+	CSingleRecipientFilter(CPlayerSlot nRecipientSlot, NetChannelBufType_t nBufType = BUF_RELIABLE, bool bInitMessage = false) :
+		CRecipientFilter(nBufType, bInitMessage)
+	{
+		if (nRecipientSlot.Get() >= 0 && nRecipientSlot.Get() < ABSOLUTE_PLAYER_LIMIT)
+			m_Recipients.Set(nRecipientSlot.Get());
+	}
 };
