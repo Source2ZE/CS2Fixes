4df810a73f7e041f6430b5b3cb9bbfb08ed2a665
Vauff <mctehkitti@gmail.com>
2025-08-04 16:15:14 -0400

    Update EmitSoundFilter

--- src/cs2_sdk/entity/cbaseentity.h
+++ src/cs2_sdk/entity/cbaseentity.h
@@ -234,12 +234,13 @@ public:
 
 	SndOpEventGuid_t EmitSoundFilter(IRecipientFilter& filter, const char* pszSound, float flVolume = 1.0, float flPitch = 1.0)
 	{
+		uint8_t unk[32];
 		EmitSound_t params;
 		params.m_pSoundName = pszSound;
 		params.m_flVolume = flVolume;
 		params.m_nPitch = flPitch;
 
-		return addresses::CBaseEntity_EmitSoundFilter(filter, entindex(), params);
+		return addresses::CBaseEntity_EmitSoundFilter(unk, filter, entindex(), params);
 	}
 
 	void DispatchParticle(const char* pszParticleName, IRecipientFilter* pFilter, ParticleAttachment_t nAttachType = PATTACH_POINT_FOLLOW,
