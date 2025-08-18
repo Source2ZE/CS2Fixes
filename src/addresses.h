4e8a87fb35fb75e339116b797c6e4b6caf6b1348
Vauff <mctehkitti@gmail.com>
2025-08-18 19:02:44 -0400

    Update SetPawn for 2025-08-18 CS2 update

--- src/addresses.h
+++ src/addresses.h
@@ -80,7 +80,7 @@ namespace addresses
 	inline void(FASTCALL* SetGroundEntity)(CBaseEntity* ent, CBaseEntity* ground, CBaseEntity* unk3);
 	inline void(FASTCALL* SetGravityScale)(CBaseEntity*, float);
 	inline void(FASTCALL* CCSPlayerController_SwitchTeam)(CCSPlayerController* pController, uint32 team);
-	inline void(FASTCALL* CBasePlayerController_SetPawn)(CBasePlayerController* pController, CCSPlayerPawn* pPawn, bool a3, bool a4, bool a5);
+	inline void(FASTCALL* CBasePlayerController_SetPawn)(CBasePlayerController* pController, CCSPlayerPawn* pPawn, bool a3, bool a4, bool a5, bool a6);
 	inline void(FASTCALL* CBaseModelEntity_SetModel)(CBaseModelEntity* pModel, const char* szModel);
 	inline void(FASTCALL* UTIL_Remove)(CEntityInstance*);
 
