fecf75bff78c6c9485c9b4d61ff0e69375e0519e
Vauff <mctehkitti@gmail.com>
2026-09-09 20:01:21 -0400

    Update signatures for 2026-09-09 CS2 update

--- src/detours.h
+++ src/detours.h
@@ -85,8 +85,8 @@ bool InitDetours(CGameConfig* gameConfig);
 void FlushAllDetours();
 bool SetupFireOutputInternalDetour();
 
-void FASTCALL Detour_UTIL_SayTextFilter(IRecipientFilter&, const char*, CCSPlayerController*, uint64);
-void FASTCALL Detour_UTIL_SayText2Filter(IRecipientFilter&, CCSPlayerController*, uint64, const char*, const char*, const char*, const char*, const char*);
+void FASTCALL Detour_UTIL_SayTextFilter(IRecipientFilter&, const char*, CCSPlayerController*, int);
+void FASTCALL Detour_UTIL_SayText2Filter(IRecipientFilter&, CCSPlayerController*, int, const char*, const char*, const char*, const char*, const char*);
 void FASTCALL Detour_TriggerPush_Touch(CTriggerPush* pPush, CBaseEntity* pOther);
 int64 FASTCALL Detour_CBaseEntity_TakeDamageOld(CBaseEntity* pThis, CTakeDamageInfo* pInfo, CTakeDamageResult* pResult);
 bool FASTCALL Detour_CCSPlayer_WeaponServices_CanUse(CCSPlayer_WeaponServices*, CBasePlayerWeapon*);
